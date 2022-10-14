812b8aebe7fcffc45ca0cde9d5fedf239b75ac7b
accent023 <codeforever@icloud.com>
2022-10-15 04:22:47 +0500

    Added favicons

--- pages/_document.js
+++ pages/_document.js
@@ -38,6 +38,29 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Jost:wght@700;800&family=Open+Sans:wght@400;600&display=swap"
             rel="stylesheet"
           />
+
+          {/*Favicon*/}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff0000" />
+          <meta name="msapplication-TileColor" content="#b91d47" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
